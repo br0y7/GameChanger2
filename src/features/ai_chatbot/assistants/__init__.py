1a79504b07916491b9b6604ce2d88f438d969b00
J Abrigo <45530276+jabrigo@users.noreply.github.com>
2026-06-03 20:20:51 -0500

    refactor: extract functions for getting assistants and data manager

--- src/features/ai_chatbot/assistants/__init__.py
+++ src/features/ai_chatbot/assistants/__init__.py
@@ -1,5 +1,13 @@
 from .protocol import AIAssistant
 from .openai_assistant import OpenAIAssistant
+import streamlit as st
+
+
+@st.cache_resource
+def get_assistants() -> list[AIAssistant]:
+    """Returns a list of assistants. Cached by Streamlit."""
+    return [OpenAIAssistant()]
+
 
 # when assistants module is imported expose classes below
 __all__ = ["AIAssistant", "OpenAIAssistant"]
