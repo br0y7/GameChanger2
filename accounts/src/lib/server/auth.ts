d65af92a049bf4d6eb0a7fda0605f19da03a53ae
J Abrigo <45530276+jabrigo@users.noreply.github.com>
2026-06-08 17:22:40 -0500

    chore: add better-auth config for dev only

--- accounts/src/lib/server/auth.ts
+++ accounts/src/lib/server/auth.ts
@@ -4,15 +4,19 @@ import { sveltekitCookies } from 'better-auth/svelte-kit';
 import { env } from '$env/dynamic/private';
 import { getRequestEvent } from '$app/server';
 import { db } from '$lib/server/db';
-import { organization } from 'better-auth/plugins';
+import { organization, openAPI } from 'better-auth/plugins';
 import type { OrgType } from '$lib/types/auth';
 
 export const auth = betterAuth({
 	baseURL: env.ORIGIN,
 	secret: env.BETTER_AUTH_SECRET,
 	database: drizzleAdapter(db, { provider: 'sqlite' }),
-	emailAndPassword: { enabled: true },
+	emailAndPassword: {
+		enabled: true,
+		requireEmailVerification: false, // FIXME: for dev only
+	},
 	plugins: [
+		openAPI(), // FIXME: maybe for dev only
 		organization({
 			teams: {
 				enabled: true,
