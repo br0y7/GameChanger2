9c3f648bf09102118a6127aec7ee486be901ecfc
J Abrigo <45530276+jabrigo@users.noreply.github.com>
2026-06-08 17:32:23 -0500

    chore: add login page components

---
+++ accounts/src/routes/auth/login/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import ModeToggle from '$lib/components/ui/ModeToggle.svelte';
+	import LoginForm from './LoginForm.svelte';
+</script>
+
+<div class="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+	<div class="absolute top-4 right-4 z-50">
+		<ModeToggle />
+	</div>
+	<div class="w-full max-w-sm">
+		<LoginForm />
+	</div>
+</div>
