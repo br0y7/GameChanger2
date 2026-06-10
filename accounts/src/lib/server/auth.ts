2ffdf4e4aa30e3e237fa73687945f2d0d4e65821
J Abrigo <45530276+jabrigo@users.noreply.github.com>
2026-06-09 19:32:45 -0500

    feat(auth): implement google button

--- accounts/src/lib/server/auth.ts
+++ accounts/src/lib/server/auth.ts
@@ -6,14 +6,22 @@ import { getRequestEvent } from '$app/server';
 import { db } from '$lib/server/db';
 import { organization, openAPI } from 'better-auth/plugins';
 import type { OrgType } from '$lib/types/auth';
+import * as schema from '$lib/server/db/schema';
+import { dev } from '$app/environment';
 
 export const auth = betterAuth({
 	baseURL: env.ORIGIN,
 	secret: env.BETTER_AUTH_SECRET,
-	database: drizzleAdapter(db, { provider: 'sqlite' }),
+	database: drizzleAdapter(db, { provider: 'sqlite', schema }),
 	emailAndPassword: {
 		enabled: true,
-		requireEmailVerification: false, // FIXME: for dev only
+		requireEmailVerification: !dev,
+	},
+	socialProviders: {
+		google: {
+			clientId: env.GOOGLE_CLIENT_ID,
+			clientSecret: env.GOOGLE_CLIENT_SECRET,
+		},
 	},
 	plugins: [
 		openAPI(), // FIXME: maybe for dev only
