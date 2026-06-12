de20ee25c484706622f8ce89c605a92391b66c02
J Abrigo <45530276+jabrigo@users.noreply.github.com>
2026-06-11 19:08:05 -0500

    feat(auth): add logout page to redirect to

---
+++ accounts/src/routes/logout/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { PUBLIC_APP_URL } from '$env/static/public';
+	import { authClient } from '$lib/auth-client';
+	import { Spinner } from '$lib/components/ui/spinner';
+
+	// Using this page so this server can remove the cookie.
+	// If you try to call the /api/auth/signout in Streamlit it
+	// won't clear the cookie.
+	$effect(() => {
+		// can't async await have to use .then()
+		authClient.signOut().then(() => window.location.replace(PUBLIC_APP_URL));
+	});
+</script>
+
+<div class="flex flex-col min-h-svh items-center justify-center">
+	<div class="w-full max-w-sm flex flex-col items-center">
+		<Spinner class="size-8" />
+		<h1 class="text-2xl">Signing out...</h1>
+	</div>
+</div>
