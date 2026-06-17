ca00e064b820d711aee4ff34142e4e16b08e2403
J Abrigo <45530276+jabrigo@users.noreply.github.com>
2026-06-17 16:34:29 -0500

    feat: add org plugin in auth client

--- platform/src/lib/auth-client.ts
+++ platform/src/lib/auth-client.ts
@@ -1,4 +1,4 @@
-import { adminClient } from 'better-auth/client/plugins';
+import { adminClient, organizationClient } from 'better-auth/client/plugins';
 import { createAuthClient } from 'better-auth/svelte';
 
-export const authClient = createAuthClient({ plugins: [adminClient()] });
+export const authClient = createAuthClient({ plugins: [adminClient(), organizationClient()] });
