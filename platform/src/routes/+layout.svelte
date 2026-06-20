683aa22b8a8d0bfad8b522f18e91696b73e69630
J Abrigo <45530276+jabrigo@users.noreply.github.com>
2026-06-19 20:02:47 -0500

    refactor: move dark mode toggle in layout

--- platform/src/routes/+layout.svelte
+++ platform/src/routes/+layout.svelte
@@ -2,9 +2,18 @@
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
 	import { ModeWatcher } from 'mode-watcher';
+	import ModeToggle from '$lib/components/ui/ModeToggle.svelte';
 	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
+
 <ModeWatcher />
-{@render children()}
+
+<div class="bg-background text-foreground min-h-screen w-full">
+	{@render children()}
+
+	<div class="absolute top-4 right-4 z-50">
+		<ModeToggle />
+	</div>
+</div>
--- platform/src/routes/login/+page.svelte
+++ platform/src/routes/login/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import ModeToggle from '$lib/components/ui/ModeToggle.svelte';
 	import LoginForm from './LoginForm.svelte';
 	import { env } from '$env/dynamic/public';
 	import type { PageProps } from './$types';
@@ -13,9 +12,6 @@
 </svelte:head>
 
 <div class="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-	<div class="absolute top-4 right-4 z-50">
-		<ModeToggle />
-	</div>
 	<div class="w-full max-w-sm">
 		<LoginForm {form} />
 	</div>
