79a73f3cceae563d9ddc3b7cbc9b63bf8372cc59
J Abrigo <45530276+jabrigo@users.noreply.github.com>
2026-06-19 20:37:29 -0500

    feat: add fade in when switching pages

--- platform/src/routes/+layout.svelte
+++ platform/src/routes/+layout.svelte
@@ -3,6 +3,9 @@
 	import favicon from '$lib/assets/favicon.svg';
 	import { ModeWatcher } from 'mode-watcher';
 	import ModeToggle from '$lib/components/ui/ModeToggle.svelte';
+	import { page } from '$app/state';
+	import { fade } from 'svelte/transition';
+
 	let { children } = $props();
 </script>
 
@@ -11,7 +14,11 @@
 <ModeWatcher />
 
 <div class="bg-background text-foreground min-h-screen w-full">
-	{@render children()}
+	{#key page.url.pathname}
+		<div in:fade={{ duration: 150 }}>
+			{@render children()}
+		</div>
+	{/key}
 
 	<div class="absolute top-4 right-4 z-50">
 		<ModeToggle />
