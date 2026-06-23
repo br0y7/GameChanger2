edcc625dce6e044bf8719b85f31cc6e9b3e10000
J Abrigo <45530276+jabrigo@users.noreply.github.com>
2026-06-22 21:12:34 -0500

    feat(onboarding): add onboarding redirect and implement clicking an onboarding card

---
+++ platform/src/routes/onboarding/+layout.server.ts
@@ -0,0 +1,45 @@
+import { redirect } from '@sveltejs/kit';
+import type { LayoutServerLoad } from './$types';
+import { resolve } from '$app/paths';
+import { PUBLIC_APP_URL } from '$env/static/public';
+import { isAdmin } from '$lib/server/guards';
+import { serverLogger } from '$lib/server/logger';
+import type { RouteId } from '$app/types';
+
+export const load: LayoutServerLoad = async ({ locals, url }) => {
+	const { user, onboarding } = locals;
+
+	if (!user || !onboarding) {
+		serverLogger.warn('No user but hit onboarding');
+		redirect(302, resolve('/login'));
+	}
+
+	if (isAdmin(user) || onboarding.status === 'complete') {
+		redirect(302, PUBLIC_APP_URL);
+	}
+
+	const BASE_ROUTE: RouteId = '/onboarding';
+
+	if (!onboarding.role || onboarding.status !== 'in_progress') {
+		if (url.pathname !== BASE_ROUTE) {
+			redirect(302, BASE_ROUTE);
+		}
+
+		return { onboarding, user };
+	}
+
+	// TODO: add the full redirects
+	const redirectMap: Record<typeof onboarding.role, RouteId> = {
+		organizer: '/onboarding/league-organizer',
+		coach: '/onboarding/coach',
+	};
+
+	const targetRoute = redirectMap[onboarding.role];
+
+	if (targetRoute && !url.pathname.startsWith(targetRoute)) {
+		serverLogger.warn(`Redirected from ${url.pathname} to ${targetRoute}`);
+		redirect(302, targetRoute);
+	}
+
+	return { onboarding, user };
+};
---
+++ platform/src/routes/onboarding/+page.server.ts
@@ -0,0 +1,62 @@
+import { resolve } from '$app/paths';
+import { db } from '$lib/server/db/index.js';
+import { userOnboarding, type Onboarding } from '$lib/server/db/schema.js';
+import { serverLogger } from '$lib/server/logger.js';
+import { error, redirect } from '@sveltejs/kit';
+import { eq } from 'drizzle-orm';
+import { z } from 'zod';
+
+const onboardingPageRoles = ['coach', 'organizer'] satisfies Onboarding['role'][];
+
+const roleSchema = z.object({
+	role: z.enum(onboardingPageRoles),
+});
+
+export type OnboardingPageRole = (typeof onboardingPageRoles)[number];
+
+export const actions = {
+	default: async ({ request, locals }) => {
+		const data = await request.formData();
+
+		const parsed = roleSchema.safeParse(Object.fromEntries(data));
+
+		if (!parsed.success || !parsed.data) {
+			serverLogger.error('Invalid data passed in /onboarding', parsed.error, parsed.data);
+			return error(500, 'Onboarding Error');
+		}
+
+		const { role } = parsed.data;
+		const { onboarding } = locals;
+
+		// This shouldn't happen, but just in case (defensive)
+		if (!onboarding) {
+			serverLogger.error(
+				'Hit /onboarding/+page.server.ts with no onboarding',
+				parsed.error,
+				parsed.data
+			);
+			return error(500, 'No onboarding on /onboarding/+page.server.ts');
+		}
+
+		await db
+			.update(userOnboarding)
+			.set({
+				role,
+				status: 'in_progress',
+			})
+			.where(eq(userOnboarding.id, onboarding.id));
+
+		serverLogger.info(`User ID: ${locals.user?.id} started onboarding`);
+
+		switch (role) {
+			case 'organizer':
+				return redirect(303, resolve('/onboarding/league-organizer'));
+			case 'coach':
+				return redirect(303, resolve('/onboarding/coach'));
+			default:
+				// For type safety, won't run unless you add a role
+				// and not add a case for it (not addressing the `satisfies` error)
+				return error(500, `${role satisfies typeof onboardingPageRoles}`);
+		}
+	},
+};
