3f10b023cb2575303a277c69487c874d3e8c4515
J Abrigo <45530276+jabrigo@users.noreply.github.com>
2026-06-30 18:33:53 -0500

    chore: enable sveltekit remote funcs

--- platform/svelte.config.js
+++ platform/svelte.config.js
@@ -5,6 +5,10 @@ const config = {
 	compilerOptions: {
 		// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
 		runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true),
+
+		experimental: {
+			async: true,
+		},
 	},
 	kit: {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
@@ -17,6 +21,10 @@ const config = {
 				...config,
 				include: [...config.include, '../drizzle.config.ts', '../scripts/**/*.ts'],
 			}),
+		},
+
+		experimental: {
+			remoteFunctions: true,
 		},
 	},
 };
