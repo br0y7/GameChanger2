78df59c321d0f11240ba23d4489b37fdd3185c41
J Abrigo <45530276+jabrigo@users.noreply.github.com>
2026-07-02 16:55:32 -0500

    refactor: simplify focusing the first error, remove unused enhance handler

--- platform/src/lib/forms/enhance.ts
+++ platform/src/lib/forms/enhance.ts
@@ -1,35 +1,17 @@
-import type { RemoteFormIssue, SubmitFunction } from '@sveltejs/kit';
+import type { RemoteFormIssue } from '@sveltejs/kit';
 import type { Attachment } from 'svelte/attachments';
 
-interface CreateEnhanceOptions {
-	onStart?: () => void;
-	onEnd?: () => void | Promise<void>;
-	onSuccess?: () => void | Promise<void>;
-}
-
 /**
- * Returns a function that handles a common pattern for `use:enhance`.
- * @param options onStart, onEnd, onSuccess callbacks
- * @returns a SubmitFunction you can use with `use:enhance`
+ * Use this using `@attach` on a container or a form that
+ * contains the fields (inputs, checkboxes, etc.).
+ * If attached, it will focus the first error.
+ *
+ * @param submitting Reactive state for when there is
+ * a form currently submitting.
+ * @param issues An optional list of all the remote form's issues.
+ * Tip: use `allIssues()` of a remote form.
+ * @returns Svelte Attachment
  */
-export function createEnhanceHandler(options: CreateEnhanceOptions): SubmitFunction {
-	return () => {
-		options.onStart?.();
-
-		return async ({ update, result }) => {
-			try {
-				await update();
-
-				if (result.type === 'success') {
-					await options.onSuccess?.();
-				}
-			} finally {
-				await options.onEnd?.();
-			}
-		};
-	};
-}
-
 export function focusFirstError({
 	submitting,
 	issues,
@@ -37,11 +19,11 @@ export function focusFirstError({
 	submitting: boolean;
 	issues?: RemoteFormIssue[];
 }): Attachment {
-	return (form) => {
+	return (container) => {
 		if (submitting || !issues?.length) {
 			return;
 		}
-		const field = form.querySelector<HTMLElement>('[aria-invalid="true"]');
-		field?.focus();
+		const element = container.querySelector<HTMLElement>('[aria-invalid="true"]');
+		element?.focus();
 	};
 }
