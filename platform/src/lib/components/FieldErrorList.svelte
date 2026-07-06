6c8c97e265d6d0a2ead76d9ff2a9a4d313e637b3
J Abrigo <45530276+jabrigo@users.noreply.github.com>
2026-07-06 14:37:11 -0500

    feat: add ability to style field errors

--- platform/src/lib/components/FieldErrorList.svelte
+++ platform/src/lib/components/FieldErrorList.svelte
@@ -2,11 +2,12 @@
 	import * as Field from '$lib/components/ui/field/index';
 	import type { RemoteFormIssue } from '@sveltejs/kit';
 	import Collapsible from './Collapsible.svelte';
+	import type { ClassValue } from 'clsx';
 
-	let { errors }: { errors?: RemoteFormIssue[] } = $props();
+	let { errors, class: className }: { errors?: RemoteFormIssue[]; class?: ClassValue } = $props();
 </script>
 
-<Collapsible isOpen={(errors ?? []).length > 0}>
+<Collapsible isOpen={(errors ?? []).length > 0} class={className}>
 	{#each errors as error (error.message)}
 		<Field.Error>{error.message}</Field.Error>
 	{/each}
