ea4343e1cb2cb8da30aac4e404e84110802e13a5
J Abrigo <45530276+jabrigo@users.noreply.github.com>
2026-07-08 19:58:28 -0500

    refactor: change redirect when logging out

--- platform/src/routes/logout/+page.svelte
+++ platform/src/routes/logout/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-	import { PUBLIC_APP_URL } from '$env/static/public';
+	import { goto } from '$app/navigation';
+	import { resolve } from '$app/paths';
 	import { authClient } from '$lib/auth-client';
 	import { Spinner } from '$lib/components/ui/spinner';
 	import { onMount } from 'svelte';
@@ -9,13 +10,13 @@
 	// won't clear the cookie.
 	onMount(async () => {
 		await authClient.signOut();
-		window.location.replace(PUBLIC_APP_URL);
+		await goto(resolve('/'));
 	});
 </script>
 
 <div class="flex flex-col min-h-svh items-center justify-center">
 	<div class="w-full max-w-sm flex flex-col items-center">
 		<Spinner class="size-8" />
-		<h1 class="text-2xl">Signing out...</h1>
+		<h1 class="text-2xl">Logging out...</h1>
 	</div>
 </div>
