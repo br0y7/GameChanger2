30e7de2ca77ee64ef4e30cf3fb778bab5f27e52d
J Abrigo <45530276+jabrigo@users.noreply.github.com>
2026-07-17 16:09:49 -0500

    feat: only show sidebar seasons navigations for league organizers

--- platform/src/routes/dashboard/NavMain.svelte
+++ platform/src/routes/dashboard/NavMain.svelte
@@ -1,22 +1,28 @@
 <script lang="ts">
 	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
-	import ShieldIcon from '@lucide/svelte/icons/shield';
 	import CalendarDaysIcon from '@lucide/svelte/icons/calendar-days';
 	import SettingsIcon from '@lucide/svelte/icons/settings';
+	import HouseIcon from '@lucide/svelte/icons/house';
 	import { getOrganization } from '$lib/api/organization.remote';
 	import { resolve } from '$app/paths';
 	import type { Pathname } from '$app/types';
+	import { isUserLeagueOrganizer } from '$lib/api/league.remote';
 
 	interface NavItem {
 		label: string;
 		href: Pathname;
-		icon: typeof ShieldIcon;
+		icon: typeof HouseIcon;
 	}
 
-	export const leagueNavigation: NavItem[] = [
-		{ label: 'Seasons', href: '/dashboard/seasons', icon: CalendarDaysIcon },
-		{ label: 'Settings', href: '/dashboard/settings', icon: SettingsIcon },
+	export const navigationItems: NavItem[] = [
+		{ label: 'Overview', href: '/dashboard', icon: HouseIcon },
 	];
+
+	if (await isUserLeagueOrganizer()) {
+		navigationItems.push({ label: 'Seasons', href: '/dashboard/seasons', icon: CalendarDaysIcon });
+	}
+
+	navigationItems.push({ label: 'Settings', href: '/dashboard/settings', icon: SettingsIcon });
 
 	const org = await getOrganization();
 </script>
@@ -24,7 +30,7 @@
 <Sidebar.Group>
 	<Sidebar.Menu>
 		{#if org.type === 'league'}
-			{#each leagueNavigation as item (item.href)}
+			{#each navigationItems as item (item.href)}
 				<Sidebar.MenuButton tooltipContent={item.label}>
 					{#snippet child({ props })}
 						<a href={resolve(item.href)} {...props}>
