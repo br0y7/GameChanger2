8c7bcaf8235bf046f62e94d5d99ad854763ba728
J Abrigo <45530276+jabrigo@users.noreply.github.com>
2026-08-09 00:33:48 -0500

    fix: restore ability to disable submit button

--- platform/src/lib/components/SubmitButton.svelte
+++ platform/src/lib/components/SubmitButton.svelte
@@ -7,15 +7,23 @@
 		children?: Snippet;
 		submitting?: boolean;
 		icon?: Snippet;
+		disabled?: boolean;
 	}
 
-	let { ref = $bindable(null), children, submitting = false, icon, ...restProps }: Props = $props();
+	let {
+		ref = $bindable(null),
+		children,
+		submitting = false,
+		icon,
+		disabled,
+		...restProps
+	}: Props = $props();
 </script>
 
 <!-- 
 Spread rest props first so it won't override the specific ones for this button.
 -->
-<Button {...restProps} bind:ref type="submit" disabled={submitting}>
+<Button {...restProps} bind:ref type="submit" disabled={submitting || disabled}>
 	{#if submitting}
 		<Spinner />
 	{:else}
