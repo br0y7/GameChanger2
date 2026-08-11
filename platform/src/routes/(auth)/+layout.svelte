031cfc7b9c2bed107f542a3839ee2634d222e117
J Abrigo <45530276+jabrigo@users.noreply.github.com>
2026-08-11 00:10:35 -0500

    feat: add dark mode toggle in login and signup pages

---
+++ platform/src/routes/(auth)/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import ModeToggle from '$lib/components/ui/ModeToggle.svelte';
+
+	let { children } = $props();
+</script>
+
+<div class="relative">
+	<div class="absolute top-2 right-2">
+		<ModeToggle />
+	</div>
+	<main>
+		{@render children()}
+	</main>
+</div>
